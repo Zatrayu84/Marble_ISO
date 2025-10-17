ed95a5ac71ededb9aa332ad0e194cbb647812764
Erik Segura <158991388+Zatrayu84@users.noreply.github.com>
2025-10-17 17:14:10 -0500

    Added powerup
    added powerup for player to get jump height increased by x2.

--- Marble_ISO_2/Assets/Scripts/PlayerController.cs
+++ Marble_ISO_2/Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -7,21 +8,31 @@ public class PlayerController : MonoBehaviour
     [SerializeField] [Range (1, 20)] float speed;
     [SerializeField] [Range(1, 20)] float jumpForce;
     
+    [Header("======== CAMERA ========")]
+    [SerializeField] Camera mainCamera;
+    public bool hasPowerup = false;
+    
     public Rigidbody playerRb;
+    public GameObject powerupIndicator;
     public float horizontalInput;
     public float verticalInput;
     private bool jumpInput;
+    
+    private Vector3 movementInput;
     
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
         MovementInput();
+        powerupIndicator.transform.position = transform.position + 
+        new Vector3(0, 1.0f, 0);
     }
 
     // FixedUpdate is called at fixed time intervals (essential for physics)
@@ -35,6 +46,7 @@ public class PlayerController : MonoBehaviour
         // Read the input values
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+        movementInput = new Vector3(horizontalInput, 0, verticalInput);
         
         if(Input.GetButtonDown("Jump"))
         {
@@ -43,16 +55,30 @@ public class PlayerController : MonoBehaviour
     }
     void MovementPhysics()
     {
-        Vector3 moveDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
-        playerRb.AddForce(moveDirection * speed, ForceMode.Acceleration);
-        
-        //This is where i can stop my sliding
-        Vector3 flatVelocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
-        
-        if(flatVelocity.magnitude > speed)
+        if (movementInput.magnitude > 0.01f)
         {
-            Vector3 limitedVelocity = flatVelocity.normalized * speed;
-            playerRb.velocity = new Vector3(limitedVelocity.x, playerRb.velocity.y, limitedVelocity.z);
+            // Get the camera's forward vector
+            Vector3 camForward = mainCamera.transform.forward;
+            camForward.y = 0;
+            camForward.Normalize(); 
+
+            // Get the camera's right vector
+            Vector3 camRight = mainCamera.transform.right;
+            camRight.y = 0;
+            camRight.Normalize();
+            
+            // this is where I get my movement direction
+            Vector3 moveDirection = (camForward * verticalInput + camRight * horizontalInput).normalized;
+            playerRb.AddForce(moveDirection * speed, ForceMode.Acceleration);
+            
+            //This is where I can stop my sliding
+            Vector3 flatVelocity = new Vector3(playerRb.velocity.x, 0, playerRb.velocity.z);
+            
+            if(flatVelocity.magnitude > speed)
+            {
+                Vector3 limitedVelocity = flatVelocity.normalized * speed;
+                playerRb.velocity = new Vector3(limitedVelocity.x, playerRb.velocity.y, limitedVelocity.z);
+            }
         }
         
         Jump();
@@ -70,5 +96,22 @@ public class PlayerController : MonoBehaviour
         {
             jumpInput = false;
         }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        powerupIndicator.gameObject.SetActive(true);
+        Debug.Log("Powerup is now turned on");
+        hasPowerup = true;
+        jumpForce *= 2;
+        StartCoroutine(powerupTimerRoutine());
+    }
+    IEnumerator powerupTimerRoutine()
+    {
+        yield return new WaitForSeconds(7);
+        hasPowerup = false;
+        jumpForce = 10.0f;
+        powerupIndicator.gameObject.SetActive(false);
     }
 }
